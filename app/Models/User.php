9e9a93cf4483c0c9d0866f7fdc731ea364980f62
RyderAsking <clasherlodt@gmail.com>
2023-07-17 22:18:41 +0530

    added code to add default plan id

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements MustVerifyEmail
      *
      * @var array<int, string>
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'plan_id'];
 
     /**
      * The attributes that should be hidden for serialization.
