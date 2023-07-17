d0b9496d1f12c107abcd5267f3f1390149d6c823
RyderAsking <clasherlodt@gmail.com>
2023-07-17 22:10:36 +0530

    created a relationship where a user belongs to a plan

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Plan;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -34,4 +36,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
 }
