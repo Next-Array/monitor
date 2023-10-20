7b9699c34a12b346147e1acc2781a9be3fc5fcd3
RyderAsking <clasherlodt@gmail.com>
2023-10-21 01:25:27 +0530

    example strucutre of db

--- app/Models/Monitor.php
+++ app/Models/Monitor.php
@@ -32,7 +32,70 @@ class Monitor extends Model
         'certificate_issuer',
         'certificate_check_failure_reason',
     ];
+    // protected $table = 'monitors';
 
+    // protected $primaryKey = 'id';
+
+    // protected $keyType = 'int';
+
+    // public $incrementing = true;
+
+    // public $timestamps = true;
+
+    // const CREATED_AT = 'created_at';
+
+    // const UPDATED_AT = 'updated_at';
+
+    // protected $attributes = [
+    //     'delayed' => false,
+    // ];
+
+    // protected $casts = [
+    //     'id' => 'int',
+    //     'delayed' => 'boolean',
+    // ];
+
+    // protected $dates = [
+    //     'created_at',
+    //     'updated_at',
+    // ];
+
+    // protected $dateFormat = 'Y-m-d H:i:s';
+
+    // protected $appends = [
+    //     'is_up',
+    // ];
+
+    // protected $with = [
+    //     'user',
+    // ];
+
+    // protected $hidden = [
+    //     'user_id',
+    // ];
+
+    // protected $visible = [
+    //     'id',
+    //     'url',
+    // ];
+
+    // protected $fillable = [
+    //     'url',
+    //     'uptime_check_enabled',
+    //     'look_for_string',
+    //     'uptime_check_interval_in_minutes',
+    //     'uptime_status',
+    //     'uptime_check_failure_reason',
+    //     'uptime_check_times_failed_in_a_row',
+    //     'uptime_status_last_change_date',
+    //     'uptime_last_check_date',
+    //     'uptime_check_failed_event_fired_on_date',
+    //     'uptime_check_method',
+    //     'uptime_check_payload',
+    //     'uptime_check_additional_headers',
+    //     'uptime_check_response_checker',
+    //     'certificate_check_enabled',
+    //
     public function user()
     {
         return $this->belongsTo(User::class);
